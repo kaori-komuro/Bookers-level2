a75812632223a6767806869590e728f29432af43
unknown <i1i1.00263@gmail.com>
2018-12-15 16:54:00 +0900

    [Update]5-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
  end
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ before_action :authenticate_user!
 
 private
 def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
 end
 
 end
